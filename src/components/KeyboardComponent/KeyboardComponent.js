729a6dfa11c36881796db46faad171bd675f9d2f
jraleman <jraleman@bendiburg.com>
2018-07-21 11:45:59 -0700

    Add dummy KeyboardComponent

---
+++ src/components/KeyboardComponent/KeyboardComponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class KeyboardComponent extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {};
+    return ;
+  }
+  render () {
+    return (
+      <div>
+        <p><{ "Dummy KeyboardComponent" }/p>
+      </div>
+    );
+  }
+}
+
+export default KeyboardComponent;
