e86677fa3408b142a887643eff5a5868c99677f9
jraleman <jraleman@bendiburg.com>
2018-07-23 14:18:02 -0700

    Add react-material-ui-keyboard example to component

--- src/components/KeyboardComponent/KeyboardComponent.js
+++ src/components/KeyboardComponent/KeyboardComponent.js
@@ -1,18 +1,38 @@
 import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import TextField from '@material-ui/core/TextField';
+import Keyboard from 'react-material-ui-keyboard';
+import { extendedKeyboard } from 'react-material-ui-keyboard/layouts';
 
 class KeyboardComponent extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = {
+      open: false,
+      value: ''
+    };
+    this.onInput = this.handleInput.bind(this);
+    return ;
+  }
+  handleInput (input) {
+    this.setState({ value: input });
     return ;
   }
   render () {
     return (
-      <div>
-        <p><{ "Dummy KeyboardComponent" }/p>
-      </div>
+      <Keyboard
+        automatic
+        onInput={this.onInput}
+        layouts={[extendedKeyboard]}
+        textField={
+          <TextField
+            id="text"
+            value={this.state.value}
+          />
+        }
+      />
     );
   }
-}
+};
 
 export default KeyboardComponent;
