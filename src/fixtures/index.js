e385773530869f4b140868935f3bea1c85517c6c
jraleman <jraleman@bendiburg.com>
2018-07-23 14:16:54 -0700

    Add fixtures source file

---
+++ src/fixtures/index.js
@@ -0,0 +1,30 @@
+const fixtures = {
+  require("./wordsList/A.json"),
+  require("./wordsList/B.json"),
+  require("./wordsList/C.json"),
+  require("./wordsList/D.json"),
+  require("./wordsList/E.json"),
+  require("./wordsList/F.json"),
+  require("./wordsList/G.json"),
+  require("./wordsList/H.json"),
+  require("./wordsList/I.json"),
+  require("./wordsList/J.json"),
+  require("./wordsList/K.json"),
+  require("./wordsList/L.json"),
+  require("./wordsList/M.json"),
+  require("./wordsList/N.json"),
+  // require("./wordsList/Ñ.json"),  <- Not implemented yet
+  require("./wordsList/O.json"),
+  require("./wordsList/P.json"),
+  require("./wordsList/Q.json"),
+  require("./wordsList/R.json"),
+  require("./wordsList/S.json"),
+  require("./wordsList/T.json"),
+  require("./wordsList/U.json"),
+  require("./wordsList/W.json"),
+  require("./wordsList/X.json"),
+  require("./wordsList/Y.json"),
+  require("./wordsList/Z.json")
+}
+
+export default fixtures;
